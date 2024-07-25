e9d7ab4c2d427e420e5a453bb8f1940fda44ba48
BerkerTomac <berker.tomac@tedu.edu.tr>
2024-07-25 16:39:27 +0300

    security fix

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <Provider store={store}>
       <App />
     </Provider>
